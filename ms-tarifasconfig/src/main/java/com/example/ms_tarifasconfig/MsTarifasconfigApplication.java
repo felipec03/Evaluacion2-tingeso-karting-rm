f8281db0c3b8625388cec2b38f19c3a5d71057e3
felipec03 <felipe.cubillos.a@usach.cl>
2025-06-05 01:33:26 -0400

    asdasd

--- ms-tarifasconfig/src/main/java/com/example/ms_tarifasconfig/MsTarifasconfigApplication.java
+++ ms-tarifasconfig/src/main/java/com/example/ms_tarifasconfig/MsTarifasconfigApplication.java
@@ -2,9 +2,7 @@ package com.example.ms_tarifasconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class MsTarifasconfigApplication {
 
