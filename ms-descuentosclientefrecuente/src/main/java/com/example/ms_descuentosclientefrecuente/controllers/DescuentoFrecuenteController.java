35e2a9d7cff85a5f6ffc7399175cd908e7b78505
felipec03 <felipe.cubillos.a@usach.cl>
2025-06-06 19:08:26 -0400

    pequeno fix

--- ms-descuentosclientefrecuente/src/main/java/com/example/ms_descuentosclientefrecuente/controllers/DescuentoFrecuenteController.java
+++ ms-descuentosclientefrecuente/src/main/java/com/example/ms_descuentosclientefrecuente/controllers/DescuentoFrecuenteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/descuentos")
+@RequestMapping("/api/descuentos")
 public class DescuentoFrecuenteController {
 
     @Autowired
--- ms-tarifadiaespecial/src/main/java/com/example/ms_tarifadiaespecial/services/TarifaDiaEspecialService.java
+++ ms-tarifadiaespecial/src/main/java/com/example/ms_tarifadiaespecial/services/TarifaDiaEspecialService.java
@@ -2,6 +2,7 @@ package com.example.ms_tarifadiaespecial.services;
 
 import com.example.ms_tarifadiaespecial.entities.TarifaDiaEspecialEntity;
 import com.example.ms_tarifadiaespecial.repositories.TarifaDiaEspecialRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -58,5 +59,28 @@ public class TarifaDiaEspecialService {
         }
 
         return precio - descuentoCumple;
+    }
+
+    @PostConstruct
+    public void inicializarFestividades() {
+        if (repository.count() == 0) {
+            // Feriados nacionales y festividades comunes en Chile
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 1, 1), "Año Nuevo", 30.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 3, 28), "Viernes Santo", 25.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 5, 1), "Día del Trabajador", 20.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 5, 21), "Día de las Glorias Navales", 20.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 6, 29), "San Pedro y San Pablo", 15.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 7, 16), "Virgen del Carmen", 15.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 8, 15), "Asunción de la Virgen", 15.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 9, 18), "Fiestas Patrias", 40.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 9, 19), "Día de las Glorias del Ejército", 40.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 10, 12), "Encuentro de Dos Mundos", 15.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 10, 31), "Día de las Iglesias Evangélicas", 10.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 11, 1), "Día de Todos los Santos", 15.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 12, 8), "Inmaculada Concepción", 20.0, true));
+            repository.save(new TarifaDiaEspecialEntity(null, LocalDate.of(2025, 12, 25), "Navidad", 35.0, true));
+            // Ejemplo de fin de semana genérico (no se usa fecha, solo para referencia)
+            repository.save(new TarifaDiaEspecialEntity(null, null, "Fin de semana", 15.0, false));
+        }
     }
 }
